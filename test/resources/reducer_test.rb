b6a4b9a777161f54110f324bd0e6b158c981a740
Jared <jaredmswanson@gmail.com>
2021-10-12 12:25:17 -0400

    Basic test created and working.

--- test/resources/reducer_test.rb
+++ test/resources/reducer_test.rb
@@ -8,23 +8,29 @@ class ReducerResourceTest < Minitest::Test
     pngs = [png]
     body = { lossy: 1, urllist: pngs }
     api_key = 'fake_key'
-    request_body = body.merge(key: api_key)
+    request_body = body.merge(key: api_key).to_json
 
-    # response = stub_response(fixture: 'reducer/results')
-    response = stub_response(fixture: 'reducer/call')
-    # stub = stub_request('reducer.php', method: :post, body: body, response: response)
-    # stub = stub_request('reducer.php', response: response, method: :post, body: body)
-    stub = stub_request('reducer.php', response: response, method: :post, body: request_body)
+    stub = Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.post('https://api.shortpixel.com/v2/reducer.php', request_body) do |env|
+        [200, {"Content-Type" => "application/json"}, File.read('test/fixtures/reducer/call.json').to_json]
+      end
+    end
 
-    # client = ShortPixel::Client.new(api_key: 'fake', adapter: :test, stubs: stub)
-    binding.pry
     client = ShortPixel::Client.new(api_key: api_key, adapter: :test, stubs: stub)
 
-    binding.pry
     reducer = client.reducer
-    # queue = reducer.call(lossy: 1, urllist: pngs)
     queue = reducer.call(**body)
 
     assert_equal ShortPixel::Reducer, queue.class
+    results = queue.results
+    assert_kind_of Array, results
+    first_result = queue.results.first
+    assert_kind_of OpenStruct, first_result
+    assert_kind_of String, first_result.original_url
+    status = first_result.status
+    assert_kind_of OpenStruct, status
+    assert_kind_of String, status.code
+    assert_kind_of String, status.message
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,18 +6,4 @@ require "json"
 require 'pry'
 
 class Minitest::Test
-  def stub_response(fixture:, status: 200, headers: {"Content-Type" => "application/json"})
-    [status, headers, File.read("test/fixtures/#{fixture}.json")]
-  end
-
-  def stub_request(path, response:, method: :get, body: {})
-    Faraday::Adapter::Test::Stubs.new do |stub|
-      # arguments = [method, "/v2/#{path}"]
-      arguments = [method, "/#{path}"]
-      # arguments = [method, "https://api.shortpixel.com/v2/reducer.php"]
-      # arguments << body.to_json if [:post, :put, :patch].include?(method)
-      arguments << body if [:post, :put, :patch].include?(method)
-      stub.send(*arguments) { |env| response }
-    end
-  end
 end
